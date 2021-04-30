da3dd366c0dfe73cf1a9fc1905180760a7fceb40
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-29 21:54:55 -0400

    Add redis sentry

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from juxta import Juxta
 import os
 import log
 import sentry_sdk
+from sentry_sdk.integrations.redis import RedisIntegration
 
 sentry_sdk.init(
     os.getenv("SENTRY_URL"),
@@ -13,9 +14,9 @@ sentry_sdk.init(
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
+    integrations=[RedisIntegration()],
 )
 log.good("Connected to Sentry")
-
 
 token = os.getenv("TOKEN")
 bot = Juxta()
