5c87b808b80121ac171c716b7e9f35dbe6b550c6
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-29 21:51:55 -0400

    Add sentry

--- run.py
+++ run.py
@@ -3,10 +3,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from juxta import Juxta
-import asyncio
-import signal
-import log
 import os
+import log
+import sentry_sdk
+
+sentry_sdk.init(
+    os.getenv("SENTRY_URL"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+log.good("Connected to Sentry")
 
 
 token = os.getenv("TOKEN")
