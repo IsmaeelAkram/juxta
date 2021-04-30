a04528ddcb925d49c8e1a115ef387d4ccf7c38d3
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-30 00:54:31 -0400

    Add hide from help option

--- command.py
+++ command.py
@@ -1,6 +1,9 @@
 class Command:
-    def __init__(self, *, name: str, description: str, usage: str, handler):
+    def __init__(
+        self, *, name: str, description: str, usage: str, handler, hide_from_help=False
+    ):
         self.name = name
         self.description = description
         self.usage = usage
         self.handler = handler
+        self.hide_from_help = hide_from_help
