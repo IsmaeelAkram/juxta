57986289a5e720d2b41cc0623794607e6e862e9e
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-29 21:04:25 -0400

    Set default embed color to white

---
+++ utils/embed.py
@@ -0,0 +1,18 @@
+import discord
+import colors
+
+
+def Embed(
+    description="",
+    image=None,
+    title="",
+    color=0xFCFCFC,
+    footer="",
+):
+    return discord.Embed(
+        description=description, image=image, title=title, color=color
+    ).set_footer(text=footer)
+
+
+def SoftErrorEmbed(error):
+    return discord.Embed(title="Oops!", description=error, color=0xD13F3F)
