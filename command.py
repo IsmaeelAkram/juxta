462078a73cfcbea2606dcba68f4fece334419032
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-29 21:51:40 -0400

    Change bool to any

--- command.py
+++ command.py
@@ -1,5 +1,5 @@
 class Command:
-    def __init__(self, *, name: str, description: str, usage: str, handler: bool):
+    def __init__(self, *, name: str, description: str, usage: str, handler):
         self.name = name
         self.description = description
         self.usage = usage
