7684d0c5fe7f4e1be23cfa883cfcbf9078d9b41c
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-30 22:40:57 -0400

    Add welcome message

---
+++ Messages.md
@@ -0,0 +1,5 @@
+## 👋 Welcome!
+I'm Juxta, I'm juxt an awesome bot for Discord! This guide introduces you to my plethora of commands and tools.
+
+## 📖 How to use Juxta
+Juxta has a [great documentation](https://ismaeelakram.github.io/juxta) for all the commands you'll need to use. If you need help with anything you can't find in the docs, you can join the [support server](https://discord.gg/5Sukasy).
--- plugins/welcome.py
+++ plugins/welcome.py
@@ -10,7 +10,34 @@ class Welcome(Plugin):
         self.client = client
         self.name = "Welcome"
         self.description = ""
-        self.commands = []
+        self.commands = [
+            Command(
+                name="test-welcome-message",
+                description="Test welcome message that sends on guild join",
+                usage="",
+                handler=self.test_welcome_message,
+                hide_from_help=True,
+            )
+        ]
+        self.hide_from_help = True
 
-    async def on_guild_join(self, guild: discord.Guild):
-        self.client.redis.sadd("juxta:guilds", guild.id)
+    async def test_welcome_message(self, args: list[str], message: discord.Message):
+        if message.author.id != 460117198795702272:
+            return
+        await self.on_guild_join(message.guild, test=True)
+
+    async def on_guild_join(self, guild: discord.Guild, test=False):
+        if not test:
+            self.client.redis.sadd("juxta:guilds", guild.id)
+        welcome_message = await self.client.redis.get("juxta:welcome-message")
+        info_message = await self.client.redis.get("juxta:info-message")
+        welcome_embed = (
+            embed.Embed(footer="Good luck!")
+            .add_field(name="👋 Welcome!", value=welcome_message.decode("utf-8"))
+            .add_field(name="📖 How to use Juxta", value=info_message.decode("utf-8"))
+        )
+
+        await guild.owner.send(embed=welcome_embed)
+
+    async def on_guild_remove(self, guild: discord.Guild):
+        self.client.redis.srem("juxta:guilds", guild.id)
