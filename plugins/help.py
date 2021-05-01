dc6469b19913571f408abd1d312a04f3e677417f
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-30 22:41:14 -0400

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -33,6 +33,7 @@ class Help(Plugin):
         )
         await message.author.send(embed=help_embed)
 
+        # TODO: Hide plugin if has attribute 'hide_from_help'
         for plugin in self.client.plugins:
             plugin_commands = ""
             for command in plugin.commands:
@@ -42,12 +43,11 @@ class Help(Plugin):
                     else:
                         usage = " " + command.usage
                     plugin_commands += f"`{self.client.PREFIX}{command.name}{usage}`\n{command.description}\n\n"
-            if len(plugin.commands) != 0:
-                await message.author.send(
-                    embed=embed.Embed(
-                        title=plugin.name, description=plugin_commands
-                    ).set_thumbnail(
-                        url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
-                    )
+            await message.author.send(
+                embed=embed.Embed(
+                    title=plugin.name, description=plugin_commands
+                ).set_thumbnail(
+                    url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
                 )
-                await message.add_reaction("✅")
+            )
+            await message.add_reaction("✅")
