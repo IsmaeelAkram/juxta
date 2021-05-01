c1507f9e5d3193bc2f81a64e6e88009f6c02873d
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-04-30 22:41:52 -0400

    Init vars in 'init' function not '__init__'

--- plugin.py
+++ plugin.py
@@ -2,10 +2,15 @@ import discord
 
 
 class Plugin:
-    def __init__(self):
+    def __init__(self, client: discord.Client):
+        self.client = client
         self.name = ""
         self.description = ""
         self.commands = []
+        self.hide_from_help = False
+
+    def init(self):
+        pass
 
     async def on_message(self, message: discord.Message):
         pass
