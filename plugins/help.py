97ee28ff63646035eba7e78ac544aa814f3bf7c1
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-05-03 00:26:48 -0400

    Clean up help.py

--- plugins/help.py
+++ plugins/help.py
@@ -2,6 +2,8 @@ from plugin import Plugin
 from command import Command
 from utils import embed
 import discord
+import storage
+import log
 
 
 class Help(Plugin):
@@ -26,38 +28,16 @@ class Help(Plugin):
         self.hide_from_help = True
 
     async def help(self, args: list[str], message: discord.Message):
+        guild_storage = storage.GuildStorage(self.client, message.guild.id)
+        guild_plugins = await guild_storage.get_plugins()
         if len(args) <= 1:
-            help_embed = embed.Embed(
-                author="Juxta Plugins Help",
-                icon="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png",
-            ).set_thumbnail(
-                url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
-            )
-            for plugin in self.client.plugins:
-                help_embed.add_field(
-                    name=plugin.name, value=f"`!help {plugin.name.lower()}`"
-                )
+            help_embed = await guild_storage.generate_main_help(guild_plugins)
             await message.author.send(embed=help_embed)
         else:
-            for plugin in self.client.plugins:
-                if plugin.slug == args[1].lower():
-                    if not plugin.hide_from_help:
-                        plugin_commands = ""
-                        for command in plugin.commands:
-                            if command.hide_from_help == False:
-                                if command.usage == "":
-                                    usage = ""
-                                else:
-                                    usage = " " + command.usage
-                                plugin_commands += f"`{command.name}{usage}`\n{command.description}\n\n"
-                        await message.author.send(
-                            embed=embed.Embed(
-                                title=plugin.name + " Plugin",
-                                description=plugin_commands,
-                            ).set_thumbnail(
-                                url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
-                            )
-                        )
-                        return
+            plugin_help_embed = await guild_storage.generate_plugin_help(
+                args, guild_plugins
+            )
+            if plugin_help_embed:
+                await message.author.send(embed=plugin_help_embed)
 
         await message.delete()
--- storage.py
+++ storage.py
@@ -2,6 +2,7 @@ import aioredis
 import discord
 from command import Command
 from plugin import Plugin
+from utils import embed
 import log
 
 
@@ -18,7 +19,13 @@ class GuildStorage:
         return await self.bot.redis.srem(self.plugins_set, plugin_slug)
 
     async def get_plugins(self):
-        return await self.bot.redis.smembers(self.plugins_set)
+        plugins = []
+        plugin_slugs = await self.bot.redis.smembers(self.plugins_set)
+        for plugin_slug in plugin_slugs:
+            for plugin in self.bot.plugins:
+                if plugin_slug == plugin.slug.encode("utf-8"):
+                    plugins.append(plugin)
+        return plugins
 
     async def has_plugin(self, plugin: Plugin):
         if plugin.slug == "help":
@@ -35,3 +42,48 @@ class GuildStorage:
                 if command in plugin.commands:
                     return True
         return False
+
+    async def generate_main_help(self, guild_plugins: [Plugin]):
+        if len(guild_plugins) == 0:
+            help_embed = embed.Embed(
+                author="Juxta Plugins Help",
+                description="You don't have any plugins enabled!",
+                icon="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png",
+            ).set_thumbnail(
+                url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
+            )
+            return help_embed
+        else:
+            help_embed = embed.Embed(
+                author="Juxta Plugins Help",
+                icon="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png",
+            ).set_thumbnail(
+                url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
+            )
+            for plugin in guild_plugins:
+                if not plugin.hide_from_help:
+                    help_embed.add_field(
+                        name=plugin.name, value=f"`!help {plugin.name.lower()}`"
+                    )
+            return help_embed
+
+    async def generate_plugin_help(self, args: list[str], guild_plugins: [Plugin]):
+        for plugin in guild_plugins:
+            if plugin.slug == args[1].lower():
+                if not plugin.hide_from_help:
+                    plugin_commands = ""
+                    for command in plugin.commands:
+                        if command.hide_from_help == False:
+                            if command.usage == "":
+                                usage = ""
+                            else:
+                                usage = " " + command.usage
+                            plugin_commands += (
+                                f"`{command.name}{usage}`\n{command.description}\n\n"
+                            )
+                    return embed.Embed(
+                        title=plugin.name + " Plugin",
+                        description=plugin_commands,
+                    ).set_thumbnail(
+                        url="https://raw.githubusercontent.com/IsmaeelAkram/juxta/master/art/Processor.png"
+                    )
